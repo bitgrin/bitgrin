7c0edae7d06d23a865d9654f982b1037bb3e79a8
pharazen <pharazen@protonmail.com>
2019-05-18 13:28:01 -0500

    Packager zip fix

--- package.sh
+++ package.sh
@@ -6,9 +6,9 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 #cargo build --release
-mkdir -p "releases/v$1/BitGrin/"
-cp ./target/release/bitgrin "releases/v$1/BitGrin/"
-cp ./bats/* "releases/v$1/BitGrin/"
-"C:\Program Files\7-Zip\7z" a -r "./releases/BitGrin-Windows-10-64Bit-$1.zip" "./releases/v$1/BitGrin"
+mkdir -p "releases/v$1/bitgrin-$1/"
+cp ./target/release/bitgrin "releases/v$1/bitgrin-$1/"
+cp ./bats/* "releases/v$1/bitgrin-$1/"
+"C:\Program Files\7-Zip\7z" a -r -tzip "./releases/BitGrin-Windows-10-64Bit-$1.zip" "./releases/v$1/bitgrin-$1"
 
 rm -rf "./releases/v$1"
