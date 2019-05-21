886e52a652bf36c5ddd0255452a1070933151109
pharazen <pharazen@protonmail.com>
2019-05-21 12:30:03 -0500

    Linux packager

--- package.sh
+++ package.sh
@@ -8,7 +8,14 @@ fi
 #cargo build --release
 mkdir -p "releases/v$1/bitgrin-$1/"
 cp ./target/release/bitgrin "releases/v$1/bitgrin-$1/"
-cp ./bats/* "releases/v$1/bitgrin-$1/"
-"C:\Program Files\7-Zip\7z" a -r -tzip "./releases/BitGrin-Windows-10-64Bit-$1.zip" "./releases/v$1/bitgrin-$1"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo 'linux'
+    cd "./releases/v$1"
+    zip -r "../BitGrin-Linux-$1.zip" "./bitgrin-$1"
+else
+    cp ./bats/* "releases/v$1/bitgrin-$1/"
+    "C:\Program Files\7-Zip\7z" a -r -tzip "./releases/BitGrin-Windows-10-64Bit-$1.zip" "./releases/v$1/bitgrin-$1"
+fi
 
 rm -rf "./releases/v$1"
